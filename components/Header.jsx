97413a5d35e06c29c0c8695a6396a098de087e42
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-06-18 18:11:22 +0300

    3rd commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,11 +3,12 @@ import { Button } from "./ui/button";
 
 //components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white  ">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items center ">
         {/*logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
@@ -15,14 +16,16 @@ const Header = () => {
           </h1>
         </Link>
         {/*Desktop nav & hire me button */}
-        <div className="hidden xl:flex">
+        <div className="hidden xl:flex items-center gap-8 ">
           <Nav />
           <Link href="/contact">
             <Button>Hire me</Button>
           </Link>
         </div>
         {/*mobile nav & hire me button */}
-        <div className="xl:hidden ">mobile nav</div>
+        <div className="xl:hidden ">
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
