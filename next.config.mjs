b32e1675816328c62b3381fd2de7b8366cfaeed2
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-07-08 08:52:55 +0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enables static HTML export
+};
 
 export default nextConfig;
