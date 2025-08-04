542d5dba56fd199445d84ad3a76fdf63e43ed957
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-08-04 17:22:28 +0300

    added socials

--- components/social.jsx
+++ components/social.jsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedin />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/HCSButcher" },
+  {
+    icon: <FaLinkedin />,
+    path: "https://www.linkedin.com/in/trevor-oduol-901b39281/",
+  },
   { icon: <FaInstagram />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  { icon: <FaTwitter />, path: "https://x.com/Trevor_oduol" },
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
